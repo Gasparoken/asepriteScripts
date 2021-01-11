2359a6b01b931ef6bc2ba80afc166e730366427d
Gaspar Capello <gaspar@igarastudio.com>
2021-01-11 12:22:48 -0300

    Fix duplicateLastFrame.lua
    Now, after running this script, the active cel is preserved.
    Code indentation changes too.

--- duplicateLastFrame.lua
+++ duplicateLastFrame.lua
@@ -1,26 +1,30 @@
-spr = app.activeSprite
+local spr = app.activeSprite
+local originalSelectedLayer = app.activeLayer
+local originalSelectedFrame = app.activeFrame
 
 function duplicatePenultimateCel(layer)
- local framesCount = #app.activeSprite.frames
- if layer:cel(framesCount-1) ~= nil then
-  local image = Image(layer:cel(framesCount-1).image)
-  spr:newCel(layer, framesCount)
-  layer:cel(framesCount).image = image
-  layer:cel(framesCount).position = layer:cel(framesCount-1).position
- end
+  local framesCount = #app.activeSprite.frames
+    if layer:cel(framesCount-1) ~= nil then
+      local image = Image(layer:cel(framesCount-1).image)
+      spr:newCel(layer, framesCount)
+      layer:cel(framesCount).image = image
+      layer:cel(framesCount).position = layer:cel(framesCount-1).position
+    end
 end
 
 app.transaction(
- function()
-  local framesCount = #app.activeSprite.frames
-  for i,layer in ipairs(spr.layers) do
-   if layer:cel(framesCount) ~= nil then
-    spr:newEmptyFrame()
+  function()
+    local framesCount = #app.activeSprite.frames
     for i,layer in ipairs(spr.layers) do
-     duplicatePenultimateCel(layer)
+      if layer:cel(framesCount) ~= nil then
+        spr:newEmptyFrame()
+        for i,layer in ipairs(spr.layers) do
+          duplicatePenultimateCel(layer)
+        end
+        app.activeLayer = originalSelectedLayer
+        app.activeFrame = originalSelectedFrame
+        break
+      end
     end
-    break
-   end
   end
- end
 )
